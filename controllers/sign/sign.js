0913b42f51c392cad1c3f718223644c9d91147ca
zhangrongwu <zhangrongwuios@icloud.com>
2019-09-29 17:51:45 +0800

    错误处理

--- controllers/sign/sign.js
+++ controllers/sign/sign.js
@@ -1,3 +1,5 @@
+
+
 module.exports = async (req, res) => {
     let info = req.body;
     try {
@@ -6,10 +8,12 @@ module.exports = async (req, res) => {
             errorcode: 200,
             msg: info
         });
+        return
     } catch (error) {
         res.json({
             errorcode: 500,
             msg: '服务器错误'
         });
+        return
     }
 }
--- controllers/userInfo/userInfo.js
+++ controllers/userInfo/userInfo.js
@@ -1,3 +1,17 @@
+const mysql = require('mysql')
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+})
+
+const bodyParser = require('body-parser')
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }))
+
 module.exports = async (req, res) => {
     let info = req.body;
     try {
@@ -6,10 +20,27 @@ module.exports = async (req, res) => {
             errorcode: 200,
             msg: info
         });
+        return
     } catch (error) {
         res.json({
             errorcode: 500,
             msg: '服务器错误'
         });
+        return
     }
+
+    const sqlStr = 'select * from userInfo where isdel=0'
+    conn.query(sqlStr, (err, results) => {
+        console.log(results)
+        if (err) return res.json({
+            err_code: 1,
+            message: '获取失败',
+            affectedRows: 0
+        })
+        res.json({
+            err_code: 0,
+            message: results,
+            affectedRows: 0
+        })
+    })
 }
