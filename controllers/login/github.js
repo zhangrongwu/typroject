36fd025d66e42b329ff62e668ff22582782f8e98
zhangrongwu <zhangrongwuios@icloud.com>
2019-10-09 18:03:41 +0800

    第三方github登录二

--- controllers/login/github.js
+++ controllers/login/github.js
@@ -1,7 +1,14 @@
+const mysql = require('mysql')
+const conn = mysql.createConnection({
+    host: '39.96.72.162',
+    user: 'zhangrongwu',
+    password: '!Ms12345678',
+    database: 'typroject'
+})
+const jwt = require('jsonwebtoken'); //用来生成token
 var superagent = require('superagent');
 
-
-module.exports = async (req, res) => {
+module.exports = async (req, Response) => {
 
     var github_access_token_url = "https://github.com/login/oauth/access_token"
     superagent.post(github_access_token_url)
@@ -13,8 +20,8 @@ module.exports = async (req, res) => {
             code: req.body.code
         })
         .end(function (err, res) {
-            var list = res.text.split("&")[0].split("=");
-            var access_token = list[1];
+            console.log("--res.body", res.body)
+            var access_token = res.body.access_token == null ? res.body.error : res.body.access_token;
             if (access_token != "bad_verification_code") {
                 var infoUrl = "https://api.github.com/user?access_token=" + access_token
                 superagent.get(infoUrl)
@@ -25,19 +32,66 @@ module.exports = async (req, res) => {
                         if (err) {
                             return console.error(err);
                         }
-                        // 获取github的用户信息
-                        return res.json({
-                            status: 1,
-                            userInfo: res.body
-                        })
+
+                        this.autoLogin(Response, res.body);
+
+                       
                     });
             } else {
-                return res.json({
+                return Response.json({
                     status: 0,
                     message: "access_token获取失败"
                 })
             }
         });
+}
 
+function autoLogin(Response, obj) {
+    var userName = obj.login;
+    conn.query(`SELECT * FROM userInfo WHERE userName='${userName}'`, (err, data) => {
+        if (err) {
+            return res.json({
+                status: 2,
+                message: "数据库错误"
+            });
+        } else {
+            if (data.length > 0) {
+                // token生成
+                let secretOrPrivateKey = "jwt"; // 这是加密的key（密钥）
+                let token = jwt.sign(userName, secretOrPrivateKey);
+                console.log("data", data)
+                if (data[0].type == "github") {
+                    return res.json({
+                        status: 1,
+                        userInfo: {
+                            token: token,
+                            userName: userName
+                        }
+                    })
+                }
 
+            } else {
+                const sqlStr = `INSERT INTO userInfo (userName, email, type) VALUES ('${username}', '${obj.email}', 'github');`
+                conn.query(sqlStr, (err, results) => {
+                    if (err) {
+                        console.log("err", err);
+                        return res.json({
+                            status: 0,
+                            msg: "数据库错误"
+                        });
+                    } else {
+                        let secretOrPrivateKey = "jwt"; // 这是加密的key（密钥）
+                        let token = jwt.sign(userName, secretOrPrivateKey);
+                        return res.json({
+                            status: 1,
+                            userInfo: {
+                                token: token,
+                                userName: userName
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    })
 }
